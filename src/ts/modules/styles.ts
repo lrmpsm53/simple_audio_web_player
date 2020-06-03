b9fc09434f1cf29f263caf091bf7f74a6585891a
DmitriyOrlov53 <orlov.dmitriy2303@outlook.com>
2020-06-03 21:31:25 +0300

    other

--- src/ts/modules/styles.ts
+++
@@ -1,4 +0,0 @@
-const styles = require('../../styles.scss');
-const styles_container = document.createElement('style');
-styles_container.innerHTML = styles.toString();
-document.head.prepend(styles_container);
