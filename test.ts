8ff44035526b13d6dd68543b862826e9eb987926
DmitriyOrlov53 <orlov.dmitriy2303@outlook.com>
2020-06-03 21:30:53 +0300

    updated dev app root

---
+++ test.ts
@@ -0,0 +1,19 @@
+import Player from './src/ts/index';
+
+const playlist = [
+    {
+        name: 'We Are The Champions',
+        source: './audio/audio1.mp3'
+    },
+    {
+        name: 'I Want To Break Free',
+        source: './audio/audio2.mp3'
+    }
+];
+
+const container = document.createElement('div');
+container.classList.add('container')
+document.body.append(container);
+
+const containers = document.getElementsByClassName('container');
+Player(containers, playlist)
